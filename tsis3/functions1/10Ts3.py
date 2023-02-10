b390e1a304daee7daba080fba13b7a7d77b01886
4Ilyas4 <122739941+4Ilyas4@users.noreply.github.com>
2023-02-10 17:12:14 +0600

    Update 10Ts3.py

--- tsis3/functions1/10Ts3.py
+++ tsis3/functions1/10Ts3.py
@@ -4,7 +4,7 @@ def uq(s):
         for j in range(i,len(s)):
             if int(s[i])==int(s[j]) and i != j:
                 b="False"
-        if(b=="True"):
-            print(int(s[i]))
+    if(b=="True"):
+        print(int(s[i]))
 l = input().split()
 uq(l)
